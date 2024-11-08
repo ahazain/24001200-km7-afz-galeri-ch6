f54225758acf02ea4639080284ec5ffd5c09dc28
ahazain <ahmadfariddd5@gmail.com>
2024-11-08 23:16:23 +0700

    problem imagekit

--- libs/imageKit.js
+++ libs/imageKit.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const ImageKit = require("imagekit");
-
+console.log("IMAGEKIT_PUBLIC_KEY:", process.env.IMAGEKIT_PUBLIC_KEY);
+console.log("IMAGEKIT_PRIVATE_KEY:", process.env.IMAGEKIT_PRIVATE_KEY);
+console.log("IMAGEKIT_URL_ENDPOINT:", process.env.IMAGEKIT_URL_ENDPOINT);
 const imageKit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLICKEY,
   privateKey: process.env.IMAGEKIT_PRIVATEKEY,
